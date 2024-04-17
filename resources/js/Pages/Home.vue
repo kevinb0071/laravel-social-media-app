9ce4982f6789bdb87a8872a0a6db6f79d31c10fc
Kevin Bonds <kevin.bonds@gmail.com>
2024-04-17 12:03:21 -0500

    Added Navigation to the page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,6 +4,7 @@ import GroupList from "@/Components/app/GroupList.vue";
 import FollowingList from "@/Components/app/FollowingList.vue";
 import PostList from "@/Components/app/PostList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 defineProps({});
 
@@ -17,19 +18,22 @@ function handleImageError() {
 
 <template>
     <Head title="Social Media App" />
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList />
+            </div>
 
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList />
-        </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowingList />
+            </div>
 
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowingList />
+            <div
+                class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col"
+            >
+                <CreatePost />
+                <PostList class="flex-1" />
+            </div>
         </div>
-
-        <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-            <CreatePost />
-            <PostList class="flex-1" />
-        </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,8 @@ Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified
 Route::get('/u/{user:username}', [ProfileController::class, 'index'])->name('profile');
 
 
-/*
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
+/*
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
